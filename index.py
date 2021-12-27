08b1a1dfb846e8fcb2710014512985034c49d057
emilianodemo <emilianodemo@gmail.com>
2021-12-27 14:09:24 -0300

    allow CORS

--- index.py
+++ index.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
+
+clients_app = ['http://localhost/3000']
 
 app = FastAPI()
 app.include_router(student_router)
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=clients_app,
+                   allow_credentials=True,
+                   allow_methods =[*],
+                   allow_headers=[*]
+                   )
